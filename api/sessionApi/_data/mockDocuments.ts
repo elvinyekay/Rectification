a9c6ce8b16bb4f10d1cb3f836a33e0835aa5e2f8
elvinyekayev <elvinyekay@gmail.com>
2025-10-30 12:16:43 +0400

    setup: vercel invalid pdf

--- api/sessionApi/_data/mockDocuments.ts
+++ api/sessionApi/_data/mockDocuments.ts
@@ -3,7 +3,7 @@ import {DocItem} from "../../../src/types/document.ts";
 export const mockDocuments: DocItem[] = [
     {
         id: "D1001",
-        fileUrl: "/scans/Alqı-satqı_müqaviləsi_Qeyri_yaşayış_sahəsi.pdf",
+        fileUrl: "/scans/test.pdf",
         customerName: "Əli Əliyev",
         type: "new",
     },
